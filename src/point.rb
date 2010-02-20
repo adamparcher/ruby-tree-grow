587436a91d4b628e769b427c685777b4448dfebe
adam.parcher <adam.parcher@7c67cce2-1517-11df-9524-c76eb015ffed>
2010-02-20 01:31:48 +0000

    added array helper for mathematical addition of array elements; added ability to calculate line segments to draw a tree (so close to drawing one!)

--- src/point.rb
+++ src/point.rb
@@ -1,5 +1,5 @@
 class Point
-	attr_accessor :x, :y, :z
+	attr_accessor :x, :y, :z, :branch
 	
 	def initialize
 		@x = 0.0
@@ -16,5 +16,9 @@ class Point
 	
 	def ==(otherPoint)
 		return (@x == otherPoint.x && @y == otherPoint.y && @z == otherPoint.z)
+	end
+	
+	def as_array
+		return [@x, @y, @z]
 	end
 end

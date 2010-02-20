63caaa47468eb0e49b77ef8244cae6190a92d8e8
adam.parcher <adam.parcher@7c67cce2-1517-11df-9524-c76eb015ffed>
2010-02-20 01:33:09 +0000

    oops, fixed bug; next time I should test before commit!

--- trunk/test/test_tree.rb
+++ trunk/test/test_tree.rb
@@ -1,5 +1,4 @@
 require 'test/unit'
-
 require 'tree'
 require 'array_helper'
 
@@ -65,27 +64,27 @@ class TestTree < Test::Unit::TestCase
 		
 		# Expected outcome of calculateLines is the list of vertices of all the line segments to draw
 		assert_equal(12, tree.vertices.size) # expect 12 vertices for 6 line segments to draw: 3 on the trunk, 3 on the branch
-		assert_equal(pointArr[0], tree.vertices[0].as_array)
-		assert_equal(pointArr[1], tree.vertices[1].as_array)
+		assert_equal(pointArr[0], tree.vertices[0])
+		assert_equal(pointArr[1], tree.vertices[1])
 		
-		assert_equal(pointArr[1], tree.vertices[2].as_array)
-		assert_equal(pointArr[2], tree.vertices[3].as_array)
+		assert_equal(pointArr[1], tree.vertices[2])
+		assert_equal(pointArr[2], tree.vertices[3])
 		
 		# Here we expect the line segments to go off for the branch....
 		# The branch points are defined relative to the place it branches off from,
 		# but the vertices we expect to be absolute points in 3D space
-		assert_equal(pointArr[4].math_add(pointArr[2]), tree.vertices[4].as_array)
-		assert_equal(pointArr[5].math_add(pointArr[2]), tree.vertices[5].as_array)
+		assert_equal(pointArr[4].math_add(pointArr[2]), tree.vertices[4])
+		assert_equal(pointArr[5].math_add(pointArr[2]), tree.vertices[5])
 		
-		assert_equal(pointArr[5].math_add(pointArr[2]), tree.vertices[6].as_array)
-		assert_equal(pointArr[6].math_add(pointArr[2]), tree.vertices[7].as_array)
+		assert_equal(pointArr[5].math_add(pointArr[2]), tree.vertices[6])
+		assert_equal(pointArr[6].math_add(pointArr[2]), tree.vertices[7])
 		
-		assert_equal(pointArr[6].math_add(pointArr[2]), tree.vertices[8].as_array)
-		assert_equal(pointArr[7].math_add(pointArr[2]), tree.vertices[9].as_array)
+		assert_equal(pointArr[6].math_add(pointArr[2]), tree.vertices[8])
+		assert_equal(pointArr[7].math_add(pointArr[2]), tree.vertices[9])
 		
 		# Before continuing on to the trunk
-		assert_equal(pointArr[2], tree.vertices[10].as_array)
-		assert_equal(pointArr[3], tree.vertices[11].as_array)
+		assert_equal(pointArr[2], tree.vertices[10])
+		assert_equal(pointArr[3], tree.vertices[11])
 	end
 	
 	def testCalculateTriangles

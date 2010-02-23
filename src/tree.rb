1e7a2d742864687e6713de1d9d8335b10c7cc48d
adam.parcher <adam.parcher@7c67cce2-1517-11df-9524-c76eb015ffed>
2010-02-23 04:12:31 +0000

    OMG we can draw a tree (loaded from YAML, simple lines)

--- src/tree.rb
+++ src/tree.rb
@@ -12,9 +12,28 @@ class Tree
 	def initialize
 		@trunk = Branch.new
 		@drawMethod = TRIANGLES
+		@treeColor = [1.0, 1.0, 1.0, 1.0]
 	end
 	
 	def calculateLines
 		self.vertices = trunk.calculateLines
+	end
+	
+	def draw
+		case (@drawMethod)
+			when LINES			# Draw some lines
+				glDisable(GL_LIGHTING)   # lines get no lighting
+				glBegin(GL_LINES)
+				glColor(@treeColor)
+				@vertices.each { |v|    # Assume vertices have been calculated for lines
+					glVertex( v[0], v[1], v[2])
+				}
+				glEnd
+				glEnable(GL_LIGHTING)
+			when WIREFRAME
+				puts "WIREFRAME: unimplemented"
+			when TRIANGLES
+				puts "TRIANGLES: unimplemented"
+		end
 	end
 end
